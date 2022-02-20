0c3ee134cafa7bcb5982ba053000795b17fe12bb
Michel Bernardo <bernardoenglish@outlook.com>
2022-02-20 15:09:06 -0300

    function refactor - new cards added - navbar settings created

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-document
-    .querySelector('.carousel-cards-section-items')
-    .addEventListener('wheel', (e) => {
-        if (e.deltaY > 0) {
-            e.target.scrollBy(341, 0);
-        } else {
-            e.target.scrollBy(-341, 0);
-        }
+const lastAddedSection = document.querySelector('#lastAddeds');
+const hypeSection = document.querySelector('#hype');
+const navbar = document.querySelector('#navbar');
+
+function carouselScroll(selectedElement) {
+    selectedElement.addEventListener(
+        'wheel',
+        (e) => {
+            e.deltaY > 0
+                ? e.target.scrollBy(350, 0)
+                : e.target.scrollBy(-350, 0);
+        },
+        { passive: true }
+    );
+}
+carouselScroll(lastAddedSection);
+carouselScroll(hypeSection);
+
+function changeNavbarBgColor(navbar) {
+    window.addEventListener('scroll', () => {
+        window.scrollY > navbar.offsetHeight
+            ? (navbar.style.background = 'black')
+            : (navbar.style.background = 'transparent');
     });
+}
+changeNavbarBgColor(navbar);
